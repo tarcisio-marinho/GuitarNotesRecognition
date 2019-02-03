058453df104d7b735f463a5e178158f76fd95757
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-02-03 16:02:27 -0300

    python dependencies

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 	http://www.philharmonia.co.uk/explore/sound_samples
 
 	http://www.philharmonia.co.uk/assets/audio/samples/guitar/guitar.zip
+
+
+# Python Dependencies
+
+	tensorflow
+	pandas
+	numpy
---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/ python3
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import pickle
+
+dataset = 'notes/'
+
+
+def preprocess():
+    pass
+
+
+def create_model():
+    pass
+    
+
+if __name__ == "__main__":
+    pass
+
+
+
+
